ddfa5b843b1334eb830a9382b9cc51e422147fef
JordanYu4 <jordanyu44@gmail.com>
2018-07-06 17:06:30 -0700

    Complete active_record_lite up to phase 1d

--- active_record_lite/lib/01_sql_object.rb
+++ active_record_lite/lib/01_sql_object.rb
@@ -5,18 +5,37 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    # ...
+    @table ||= DBConnection.execute2(<<-SQL)
+      SELECT
+        *
+      FROM
+        "#{self.table_name}"
+    SQL
+    @table.first.map { |col| col.to_sym }
   end
 
   def self.finalize!
+    self.columns.each do |col|
+
+      define_method(col) do
+        self.attributes
+        @attributes[col]
+      end
+
+      define_method("#{col.to_s}=") do |val|
+        self.attributes
+        @attributes[col] = val
+      end
+
+    end
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
   end
 
   def self.table_name
-    # ...
+    @table_name ||= self.to_s.tableize
   end
 
   def self.all
@@ -32,11 +51,16 @@ class SQLObject
   end
 
   def initialize(params = {})
-    # ...
+    params.each do |attr_name, value|
+      raise "Unknown attribute #{attr_name}" unless self.class.columns.include?(attr_name)
+      self.send("#{attr_name}=".to_sym, value)
+    end
   end
 
   def attributes
-    # ...
+    @attributes ||= {}
+    @attributes
+
   end
 
   def attribute_values
